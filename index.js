5a3e8bea5d970ec8d285f8b3f7ff0f71213efb80
AirFusion <jfang.cv.ca.us@gmail.com>
2020-12-25 15:48:11 -0800

    need to fix info section, but the cheerio broken issue is fixed

--- index.js
+++ index.js
@@ -66,26 +66,39 @@ app.get('/results', (req, resApp) => {
         .end((err, res) => {
             // console.log(res.text)
             var $ = cheerio.load(res.text)
-            console.log($("table", ".results.screens").length)
+            // console.log($("div .nospace", ".main .results.screens").filter('h4'))
 
-            for (i = 0; i < $("table", ".main .results.screens").length; i++) { // number of "History at"s -> 2 - might be broken
+            for (i = 0; i < $("div .nospace", ".main .results.screens").filter('h4').length; i++) { // number of "History at"s -> 2
                 // resData[$("div .nospace", ".main .results.screens").filter('h4')[i].innerText] = null // create blank object with key as the "history at..."
-                resData += `${$("div .nospace", ".main .results.screens").filter('h4')[i].innerText}: {` // need closing }, after adding center core below. also need a {} to wrap the json
-                for (j = 0; j < $("table", ".main .results.screens")[i].children[1].children.length-1; j++) { // number of rows -> 9 OR 2
+                // console.log($("div .nospace", ".main .results.screens").filter('h4')[i].children.find(child => child.type == 'text').data.trim())
+                resData += `${$("div .nospace", ".main .results.screens").filter('h4')[i].children.find(child => child.type == 'text').data.trim()}: {` // need closing }, after adding center core below. also need a {} to wrap the json
+
+                // console.log(($("table", ".main .results.screens")[1].children.find(child => child.name == 'tbody').children.length-1)/2)
+
+                for (j = 0; j < ($("table", ".main .results.screens")[i].children.find(child => child.name == 'tbody').children.length-1)/2; j++) { // number of rows -> 9 OR 2  // might need a forloop searching for tr elements later on, but seems like its always 2x the actual number +1. 
+
                     // var tournamentObject = {
                     //     : ""
                     // }
                     // $("table", ".main .results.screens")[0].children[1].children[j].children[0].innerText
                     // resData[$("div .nospace", ".main .results.screens").filter('h4')[i].innerText] += JSON.parse(`${$("table", ".main .results.screens")[0].children[1].children[j].children[0].innerText}: {"info": {},"rounds": {}}`) // use json.parse()
-                    console.log($("table", ".main .results.screens")[i].children[1].children.length)
-                    console.log(j)
-                    console.log($("table", ".main .results.screens")[0].children[1].children[j]) // <---
-                    resData += `${$("table", ".main .results.screens")[0].children[1].children[j].children[0].innerText}: { "info": {`
+                    // console.log($("table", ".main .results.screens")[i].children[1].children.length)
+                    // console.log(j)
+                    // console.log($("table", ".main .results.screens")[0].children.find(child => child.name == 'tbody').children.find(child => child.name == 'tr').children.find(child => child.name == 'td'))
+                    // console.log($("table", ".main .results.screens")[0].children.find(child => child.name == 'tbody').children.find(child => child.name == 'tr').children.find(child => child.name == 'td').children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()) 
+                    // console.log($("table", ".main .results.screens")[0].children[1].children[j].next)
+                    // console.log($("table", ".main .results.screens")[0].children[1].children[j].parent.parent.children.find(child => child.name == 'tbody').children.find(child => child.name == 'tr'))
+                    // console.log($($("table", ".main .results.screens")[i].children.find(child => child.name == 'tbody')).children('tr')[j].children.find(child => child.name == 'td').children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim())
+
+                    resData += `${$($("table", ".main .results.screens")[i].children.find(child => child.name == 'tbody')).children('tr')[j].children.find(child => child.name == 'td').children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()}: { "info": {` // -> DDI Tournament
 
                     // for (k = 0; k < $("table", ".main .results.screens")[2].children[1].children[j].children.length; k++) { // number of fields with in a row, starting with tournament name -> 5
                     // info part
+
                     resData += `"name":{${$("table", ".main .results.screens")[0].children[1].children[j].children[0].innerText}}, "date": {${$("table", ".main .results.screens")[0].children[1].children[0].children[1].innerText.trim()}}, "code": {${$("table", ".main .results.screens")[0].children[1].children[0].children[2].innerText.trim()}}, "division":{${$("table", ".main .results.screens")[0].children[1].children[0].children[3].innerText.trim()}}, "resultsLink":{${$("table", ".main .results.screens")[0].children[1].children[0].children[4].children[0].href}}}` // extra } to close info section
                     // }
+
+                    //superagent requests to each tournament's results pg, then forloop the number of rounds
 
                 }
                 resData+=`}` // close "history at... sectioin"
