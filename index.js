9892243fb3d59053b991f7f5a76fb9a6e7cf5765
AirFusion <jfang.cv.ca.us@gmail.com>
2021-08-02 23:10:17 -0700

    pushing as I go so I don't destroy anything
    Original: ~1103.71 ms (avg of 7 tries back to back w/ ~3-5 sec delay between)
    New: ~409.2ms (avg of 5 w/ ~3-5 sec delay between requests)

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
  * @todo app.get('/me/current') -> current entries (ref old html saves of active entries?)
  */
 
-async function tabroomTokenTest(req, resApp) {
+function tabroomTokenTest(req, resApp) {
 
     return new Promise((resolve, reject) => {
         superagent
@@ -108,76 +108,11 @@ app.post('/me', async function (req, resApp) {
         resApp.send('Invalid API Key or no authentication provided.')
         return;
     }
+    const { nsda, profile } = require('./modules/me')
 
-    nsda(req, resApp)
-    async function nsda(req, resApp) {
-        superagent
-            .get('https://www.tabroom.com/user/student/nsda.mhtml')
-            .set("Cookie", req.body.token)
-            .end(async (err, res) => {
-                var $ = cheerio.load(res.text)
-
-                var userInfo = {
-                    'nsdaMemberNumber': null,
-                    'nsdaPoints': null,
-                    'districtTournament': null,
-                    'nsdaAffiliation': null,
-                    'latestNsdaHonor': null,
-                    'latestNsdaHonorDate': null,
-
-                    'nameFirst': null,
-                    'nameLast': null,
-                    'email': null,
-                    'pronouns': null
-                }
-
-                userInfo.nsdaMemberNumber = $($($('#content .main').children('div')[0]).children('span')[0]).text().trim().replace(/Member/g, '').replace(/#/g, '').trim()
-
-                if (userInfo.nsdaMemberNumber.includes('Upcoming Tournaments')) {
-                    // token out of date
-                    resApp.status(403)
-                    resApp.send(`Tabroom.com token is out of date, please run /login again to get token.`)
-                    return;
-                }
-
-                userInfo.nsdaPoints = $($($('#content .main').children('div')[0]).children('span')[1]).children('div')[1].children.find(child => child.type == 'text').data.trim().replace(/\D/g, '')
-
-                userInfo.districtTournament = ($($($('#content .main').children('div')[0]).children('span')[1]).children('div')[2].children.find(child => child.type == 'text').data.trim().replace(/\t/g, "").replace(/\n/g, " ").toLowerCase().includes('you are eligible') ? userInfo.districtTournament = true : userInfo.districtTournament = false)
-
-                userInfo.nsdaAffiliation = $($($($('#content .main').children('div')[1]).children('span')[0]).children('div')[0]).children('span')[3].children.find(child => child.type == 'text').data.trim()
-
-                userInfo.latestNsdaHonor = $($($($('#content .main').children('div')[1]).children('span')[0]).children('div')[0]).children('span')[1].children.find(child => child.type == 'text').data.trim()
-
-                userInfo.latestNsdaHonorDate = $($($($('#content .main').children('div')[1]).children('span')[0]).children('div')[0]).children('span')[2].children.find(child => child.type == 'text').data.trim()
-
-                userInfo = await profile(req, userInfo)
-
-                resApp.send(userInfo)
-            })
-    }
-
-    async function profile(req, userInfo) {
-        return new Promise((resolve, reject) => {
-            superagent
-                .get('https://www.tabroom.com/user/login/profile.mhtml')
-                .set("Cookie", req.body.token)
-                .end((err, res) => {
-                    var $ = cheerio.load(res.text)
-
-                    // history @ 0, tournament row @ 0
-                    userInfo.nameFirst = $($('[name="first"]')[0]).attr('value')
-
-                    userInfo.nameLast = $($('[name="last"]')[0]).attr('value')
-
-                    userInfo.email = $($('[name="email"]')[0]).attr('value')
-
-                    userInfo.pronouns = $($('[name="pronoun"]')[0]).attr('value')
-
-                    resolve(userInfo)
-                })
-        })
-    }
-
+    Promise.all([nsda(req), profile(req)]).then((val) => {
+        resApp.send({ ...val[0], ...val[1] })
+    })
 })
 
 app.post('/me/results', async function (req, resApp) {
