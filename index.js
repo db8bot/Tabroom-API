81496493295143513d61680f50bce92f847b713e
AirFusion <jfang.cv.ca.us@gmail.com>
2021-08-04 11:25:50 -0700

    /paradigm fix done.
    old: 759.8ms (avg of 5 runs)
    new: 548.6ms (avg of 5 runs)

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ app.post('/me/current', function (req, resApp) { // docs - input token & api aut
         return;
     }
     const { basicTournamentInfo } = require('./modules/current')
-    Promise.all([basicTournamentInfo(req.body.token)]).then(val => {
+    Promise.all([basicTournamentInfo(req.body.token)]).then(val => { // could use async await here
         if (val[0] === 204) {
             resApp.sendStatus(204)
         } else {
@@ -250,7 +250,6 @@ app.post('/paradigm', (req, resApp) => {
         resApp.send('Invalid API Key or no authentication provided.')
         return;
     }
-
     var requestLink = ""
     if (req.body.type === 'name') {
         requestLink = `https://www.tabroom.com/index/paradigm.mhtml`
@@ -259,216 +258,12 @@ app.post('/paradigm', (req, resApp) => {
     } else if (req.body.type === 'link') {
         requestLink = req.body.link
     }
-
-    if (req.body.first != undefined) {
-        superagent
-            .post('https://www.tabroom.com/index/paradigm.mhtml')
-            .set('Content-Type', 'application/x-www-form-urlencoded')
-            .redirects(0)
-            .send(JSON.parse(`{"search_first": "${req.body.first}", "search_last": "${req.body.last}"}`))
-            .end(async (err, res) => {
-                var $ = cheerio.load(res.text)
-                var returnData = []
-                var judgeLink = ""
-                /**
-                 * loop: i->table len. Count votes while stuffing tournament info in json objs stuffed in an array
-                 */
-                if (res.text.includes(`returned no judges with paradigms.`)) {
-                    resApp.sendStatus(204)
-                    return;
-                }
-                if (res.text.includes(`Paradigm search results`)) { // multiple results from search
-                    for (x = 0; x < $("#paradigm_search").children('tbody').children('tr').length; x++) { // check how many results would result
-                        judgeLink = "https://www.tabroom.com/index/" + $($($("#paradigm_search").children('tbody').children('tr')[x]).children('td')[3]).children('a')[0].attribs.href
-                        returnData.push(await paradigmSplitter($, req, resApp, judgeLink))
-                    }
-                    resApp.send(returnData)
-                } else {
-                    returnData = await paradigmWork(req, $, resApp)
-                    resApp.send(returnData)
-                }
-
-            })
-
-
-        function paradigmSplitter($, req, resApp, judgeLink) {
-            return new Promise((resolve, reject) => {
-                superagent
-                    .get(judgeLink)
-                    // .redirects(0)
-                    .end(async (err, resEachParadigm) => {
-                        // console.log(resEachParadigm.text)
-                        selectorResEachParadigm = cheerio.load(resEachParadigm.text)
-                        resolve(await paradigmWork(req, selectorResEachParadigm, resApp, judgeLink))
-                    })
-            })
-        }
-
-        async function paradigmWork(req, $, resApp, judgeLink) {
-            return new Promise((resolve, reject) => {
-                var roundJudgedInfo = null
-                var judgeRecord = []
-                judgeRecord.push($('.paradigm').text())
-                judgeRecord.push($('.paradigm').html().replace(/<p>/gmi, "").replace(/<\/p>/gmi, ""))
-                if (judgeLink != null) judgeRecord.push(judgeLink)
-                if (req.body.short == 'true') {
-                    // resApp.send(judgeRecord)
-                    // return;
-                    resolve(judgeRecord)
-                    return;
-                }
-
-                // round judged limit 
-                var roundJudgedLen = null
-                if (req.body.roundLimit != undefined) {
-                    if (req.body.roundLimit > 200) {
-                        roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), 200)
-                    } else if (req.body.roundLimit <= 200) {
-                        roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), req.body.roundLimit)
-                    }
-                } else {
-                    roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), 200)
-                }
-
-                for (i = 0; i < roundJudgedLen; i++) {
-                    roundJudgedInfo = {
-                        "tournament": "",
-                        "date": "",
-                        "timestamp": "",
-                        "event": "",
-                        "round": "",
-                        "affTeamCode": "",
-                        "negTeamCode": "",
-                        "judgeVote": "",
-                        "result": ""
-                    }
-
-                    if (req.body.basecamp == 'true') {
-                        roundJudgedInfo.timestamp = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.judgeVote = $($('#record').children('tbody').children('tr')[i]).children('td')[7].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.event = $($('#record').children('tbody').children('tr')[i]).children('td')[3].children.find(child => child.type == 'text').data.trim()
-                    } else {
-                        roundJudgedInfo.tournament = $($('#record').children('tbody').children('tr')[i]).children('td')[0].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.level = $($($('#record').children('tbody').children('tr')[i]).children('td')[1]).text().trim()
-
-                        roundJudgedInfo.date = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').next.data.trim()
-
-                        roundJudgedInfo.timestamp = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.event = $($('#record').children('tbody').children('tr')[i]).children('td')[3].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.round = $($('#record').children('tbody').children('tr')[i]).children('td')[4].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.affTeamCode = $($('#record').children('tbody').children('tr')[i]).children('td')[5].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.negTeamCode = $($('#record').children('tbody').children('tr')[i]).children('td')[6].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.judgeVote = $($('#record').children('tbody').children('tr')[i]).children('td')[7].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.result = $($('#record').children('tbody').children('tr')[i]).children('td')[8].children.find(child => child.type == 'text').data.trim()
-
-                    }
-
-                    /** Debugging
-                     * break;
-                     */
-
-                    judgeRecord.push(roundJudgedInfo)
-
-                }
-                resolve(judgeRecord)
-            })
-        }
-    }
-    else {
-        superagent
-            .get(requestLink)
-            .redirects(0)
-            .end((err, res) => {
-                var $ = cheerio.load(res.text)
-                /**
-                 * loop: i->table len. Count votes while stuffing tournament info in json objs stuffed in an array
-                 */
-                if (res.text.includes(`returned no judges with paradigms.`)) {
-                    resApp.sendStatus(204)
-                    return;
-                }
-                var roundJudgedInfo = null
-                var judgeRecord = []
-                judgeRecord.push($('.paradigm').text())
-                judgeRecord.push($('.paradigm').html().replace(/<p>/gmi, "").replace(/<\/p>/gmi, ""))
-
-                if (req.body.short == 'true') {
-                    resApp.send(judgeRecord)
-                    return;
-                }
-
-                // round judged limit to 170 rounds
-                if (req.body.roundLimit != undefined) {
-                    if (req.body.roundLimit > 200) {
-                        roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), 200)
-                    } else if (req.body.roundLimit <= 200) {
-                        roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), req.body.roundLimit)
-                    }
-                } else {
-                    roundJudgedLen = Math.min(parseInt($('#record').children('tbody').children('tr').length), 200)
-                }
-
-                for (i = 0; i < roundJudgedLen; i++) {
-                    roundJudgedInfo = {
-                        "tournament": "",
-                        "level": "",
-                        "date": "",
-                        "timestamp": "",
-                        "event": "",
-                        "round": "",
-                        "affTeamCode": "",
-                        "negTeamCode": "",
-                        "judgeVote": "",
-                        "result": ""
-                    }
-
-                    if (req.body.basecamp == 'true') {
-                        roundJudgedInfo.timestamp = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.judgeVote = $($('#record').children('tbody').children('tr')[i]).children('td')[7].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.event = $($('#record').children('tbody').children('tr')[i]).children('td')[3].children.find(child => child.type == 'text').data.trim()
-                    } else {
-                        roundJudgedInfo.tournament = $($('#record').children('tbody').children('tr')[i]).children('td')[0].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.level = $($($('#record').children('tbody').children('tr')[i]).children('td')[1]).text().trim()
-
-                        roundJudgedInfo.date = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').next.data.trim()
-
-                        roundJudgedInfo.timestamp = $($('#record').children('tbody').children('tr')[i]).children('td')[2].children.find(child => child.name == 'span').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.event = $($('#record').children('tbody').children('tr')[i]).children('td')[3].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.round = $($('#record').children('tbody').children('tr')[i]).children('td')[4].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.affTeamCode = $($('#record').children('tbody').children('tr')[i]).children('td')[5].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.negTeamCode = $($('#record').children('tbody').children('tr')[i]).children('td')[6].children.find(child => child.name == 'a').children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.judgeVote = $($('#record').children('tbody').children('tr')[i]).children('td')[7].children.find(child => child.type == 'text').data.trim()
-
-                        roundJudgedInfo.result = $($('#record').children('tbody').children('tr')[i]).children('td')[8].children.find(child => child.type == 'text').data.trim()
-
-                    }
-
-                    /** Debugging
-                     * break;
-                     */
-
-                    judgeRecord.push(roundJudgedInfo)
-                }
-                resApp.send(judgeRecord)
-            })
-    }
+    const { start } = require('./modules/paradigm')
+    Promise.all([start(req, requestLink)]).then(val => {
+        resApp.send(val[0])
+    }).catch(err => {
+        console.error(err)
+    })
 })
 
 app.post('/upcoming', (req, resApp) => {
