ee9094d3fbcfd43dc8aee0bdca3921c5a12bf4c1
AirFusion45 <jfang.cv.ca.us@gmail.com>
2022-12-02 17:38:18 -0800

    working on paradigm scrape

--- dev/paradigmexp.js
+++ dev/paradigmexp.js
@@ -1,7 +1,9 @@
 const superagent = require('superagent')
 const cheerio = require('cheerio')
+const { Tabletojson: tabletojson } = require('tabletojson')
 
-const requestLink = 'https://www.tabroom.com/index/paradigm.mhtml?judge_person_id=185947'
+// const requestLink = 'https://www.tabroom.com/index/paradigm.mhtml?judge_person_id=185947'
+const requestLink = 'https://www.tabroom.com/index/paradigm.mhtml?judge_person_id=6631'
 
 
 superagent
@@ -9,9 +11,13 @@ superagent
     .set('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36')
     .end((err, res) => {
         if (err) return resApp.status(500).send(err)
-        const $ = cheerio.load(res.text)
 
-        let paradigmText = $('.paradigm.ltborderbottom').text().trim()
-
-        console.log(paradigmText)
+        
     })
+        // const $ = cheerio.load(res.text)
+
+        // let paradigmText = $('.paradigm.ltborderbottom').text().trim()
+        // const converted = tabletojson.convert(res.text)
+
+        // console.log(paradigmText)
+        // console.log(converted)
--- routes/paradigm.js
+++ routes/paradigm.js
@@ -2,6 +2,7 @@ const cheerio = require('cheerio')
 const express = require('express')
 const router = express.Router()
 const superagent = require('superagent')
+const { Tabletojson: tabletojson } = require('tabletojson')
 
 const MongoClient = require('mongodb').MongoClient
 const uri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@db8botcluster.q3bif.mongodb.net/23bot?retryWrites=true&w=majority`
@@ -20,20 +21,50 @@ router.post('/', async (req, resApp) => {
         requestLink = req.body.link
     }
 
-    if (requestLink) {
+    if (req.body.type === 'name') {
+        superagent
+            .post(requestLink)
+            .set('User-Agent', useragent)
+            .send({ search_first: req.body.first, search_last: req.body.last })
+            .end(async (err, res) => {
+                if (err) return resApp.status(500).send(err)
+                await paradigmProcessing(res.text)
+            })
+    } else if (requestLink) {
         superagent
             .get(requestLink)
-            .set('User-Agent', useragent) // if by name, send post ruqest- need a nother header and condintoality
-            .end((err, res) => {
+            .set('User-Agent', useragent)
+            .end(async (err, res) => {
                 if (err) return resApp.status(500).send(err)
-                const $ = cheerio.load(res.text)
+                // check if there are multiple people or no paradigms
+                if (res.text.includes('returned no judges. Please try again.')) {
+                    console.log('no paradigms found')
+                    resApp.status(404).send('no paradigms found')
+                } else if (res.text.includes('Paradigm search results')) {
+                    console.log('multiple people')
+                    const $ = cheerio.load(res.text)
 
-                let paradigmText = $('.paradigm.ltborderbottom').text().trim()
+                    for (i = 0; i < $($($('#paradigm_search')[0]).children('tbody')[0]).children('tr').length; i++) {
+                        superagent
+                        .get(`https://www.tabroom.com${}`)
+                        await paradigmProcessing(res.text)
+                    }
+                } else {
 
-                console.log(paradigmText)
+                }
+
             })
     }
 
 })
+
+async function paradigmProcessing(html) {
+    const $ = cheerio.load(html)
+    let paradigmText = $('.paradigm.ltborderbottom').text().trim()
+
+    const converted = tabletojson.convert(paradigmText)
+    // console.log(paradigmText)
+    console.log(converted)
+}
 
 module.exports = router
